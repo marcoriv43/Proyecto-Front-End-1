45ade89f94934976e64ec35afc9bec15dd6de5b7
Marco Rivero <marco.riv43@gmail.com>
2025-04-09 02:30:51 +0000

    Mejoras

--- src/Views/login.jsx
+++ src/Views/login.jsx
@@ -1,86 +1,80 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 export const Login = () => {
-
   return (
     <div className="w-full h-screen grid grid-cols-2">
-    <div className="flex items-center justify-center bg-gray-200">
-    <div className="md:w-1/2 px-8 md:px-16">
-        <h2 className="font-bold text-2xl text-[#002D74]">Iniciar sesion</h2>
-        <p className="text-xs mt-4 text-[#002D74]">
+      <div className="flex items-center justify-center bg-gray-200">
+        <div className="md:w-1/2 px-8 md:px-16">
+          <h2 className="font-bold text-2xl text-[#002D74]">Iniciar sesion</h2>
+          <p className="text-xs mt-4 text-[#002D74]">
             Si ya eres miembro, inicia sesión fácilmente
-        </p>
+          </p>
 
-        {/* form */}
-        <form action="" className="flex flex-col gap-4">
-          <input
-            className="p-2 mt-8 rounded-xl border"
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
-          <div className="relative">
-            <input
-              className="p-2 rounded-xl border w-full"
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
+          {/* form */}
+          {LoginForm /}
+
+          <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+            <a href="#">Olvide mi contraseña</a>
           </div>
-          <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">
-            Login
-          </button>
-        </form>
 
-
-
-        <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
-          <a href="#">Olvide mi contraseña</a>
-        </div>
-
-        {/* REGISTRARSE */}
-        <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
-          <p>¿No tienes una cuenta?</p>
-          <Link to={"/register"}>
-            <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Registrarse</button>
-          </Link>
+          {/* REGISTRARSE */}
+          <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+            <p>¿No tienes una cuenta?</p>
+            <Link to={"/register"}>
+              <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
+                Registrarse
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
+      <div>
+        <img src="/login.jpg" className="w-full h-full object-fill" />
+      </div>
     </div>
-    <div>
-      
-      <img src="/login.jpg" className="w-full h-full object-fill"/>
-    </div>
-  </div>
-  )
-}
-
+  );
+};
 
 export const Registro = () => {
+  return (
+    <div class="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+      <p>Don't have an account?</p>
+      <Link to={"/register"}>
+        <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
+          Register
+        </button>
+      </Link>
+    </div>
+  );
+};
 
-  return (   
-  <div class="mt-3 text-xs flex justify-between items-center text-[#002D74]">
-    <p>Don't have an account?</p>
-    <Link to={"/register"}>
-    <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Register</button>
-    </Link>
-  </div>
-  )
-}
-
-
-
-export default Login
-
-
+export default Login;
 
 const LoginForm = () => {
 
-  
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  console.log(watch("example"))
 
   return (
-    <form>
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
 
+      <input className="p-2 mt-8 rounded-xl border" {...register("email", { 
+        required: true, 
+        pattern: 
+      })} />
+
+  
+      <input className="p-2 rounded-xl border w-full" {...register("password", { required: true })} />        
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input  className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300" type="submit" />
     </form>
-  )
-}
+    )
+};
--- src/Views/register.jsx
+++ src/Views/register.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 const Register = () => {
   return (
@@ -8,24 +8,19 @@ const Register = () => {
           <h2 className="font-bold text-2xl text-[#002D74]">Crea una cuenta</h2>
           {/* form */}
           <form action="" className="flex flex-col gap-4">
+            <input
+              className="p-2 rounded-xl border w-full"
+              type="name"
+              name="name"
+              placeholder="name"
+            />
 
-
-              <input
-                className="p-2 rounded-xl border w-full"
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-
-
-            <div className="relative">
             <input
               className="p-2 mt-8 rounded-xl border"
               type="email"
               name="email"
               placeholder="Email"
             />
-            </div>
 
             <div className="relative">
               <input
@@ -36,13 +31,10 @@ const Register = () => {
               />
             </div>
 
-
             <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">
               Login
             </button>
           </form>
-
-
 
           <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
             <a href="#">Olvide mi contraseña</a>
@@ -52,23 +44,32 @@ const Register = () => {
           <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
             <p>¿No tienes una cuenta?</p>
             <Link to={"/register"}>
-              <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Registrarse</button>
+              <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
+                Registrarse
+              </button>
             </Link>
           </div>
         </div>
       </div>
       <div>
-
         <img src="/login.jpg" className="w-full h-full object-fill" />
       </div>
     </div>
-  )
+  );
+};
+
+{
+  /* COLUMNA PARA FORMULARIO */
+}
+{
+  /* form*/
 }
 
-{/* COLUMNA PARA FORMULARIO */ }
-{/* form*/ }
+{
+  /* Iniciar sesion con google */
+}
 
-{/* Iniciar sesion con google */ }
-
-{/* COLUMNA PARA IMAGEN */ }
-export default Register
+{
+  /* COLUMNA PARA IMAGEN */
+}
+export default Register;
